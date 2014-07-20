2117338eda5cb7806ae6e49ca5ec37ff95f0e347
Nami-Doc <vendethiel@hotmail.fr>
2014-07-20 22:45:45 +0200

    module is not like package, it can be filescoped

--- lib/Sprockets.pm
+++ lib/Sprockets.pm
@@ -1,23 +1,23 @@
-module Sprockets {
-	my @extensions = <js html css txt
-		png gif jpg jpeg
-		otf ttf>;
+module Sprockets;
+my @extensions = <js html css txt
+	png gif jpg jpeg
+	otf ttf>;
 
-	our sub split-name-and-extension($filename) is export {
-		my Str $type;
-		my @parts = do for $filename.split('.') {
-			# todo : uncomment that when rakudo fixes the regression
-			#LAST { $type = $_ }
-			#last if $_ eq any(@extensions);
-			if $_ eq any(@extensions) {
-				$type = $_;
-				last;
-			}
-
-			$_
+our sub split-name-and-extension($filename) is export {
+	my Str $type;
+	my @parts = do for $filename.split('.') {
+		# todo : uncomment that when rakudo fixes the regression
+		#LAST { $type = $_ }
+		#last if $_ eq any(@extensions);
+		if $_ eq any(@extensions) {
+			$type = $_;
+			last;
 		}
-		fail "Missing file extension for $filename" unless $type;
 
-		(@parts.join('.'), $type)
+		$_
 	}
+	fail "Missing file extension for $filename" unless $type;
+
+	(@parts.join('.'), $type)
+}
 }
